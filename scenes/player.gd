2321b16c08d40e62fa82b9cd7188d3523ad5123f
damianhaq <40294540+damianhaq@users.noreply.github.com>
2024-12-28 21:21:20 +0100

    flip texture

--- scenes/player.gd
+++ scenes/player.gd
@@ -3,9 +3,6 @@ extends CharacterBody2D
 const SPEED = 100
 
 func _physics_process(delta: float) -> void:
-	# Handle jump.
-	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		#velocity.y = JUMP_VELOCITY
 
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_axis("left", "right")
@@ -16,5 +13,12 @@ func _physics_process(delta: float) -> void:
 		velocity = input_vector * SPEED
 	else:
 		velocity = input_vector
+		
+		
+	# flip texture
+	if input_vector.x < 0:
+		$AnimatedSprite2D.flip_h = true
+	if input_vector.x > 0:
+		$AnimatedSprite2D.flip_h = false
 
 	move_and_slide()
