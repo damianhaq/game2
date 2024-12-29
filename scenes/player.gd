413eeb9f052f3a5ee56168a201af1fbd93b93008
damianhaq <40294540+damianhaq@users.noreply.github.com>
2024-12-29 11:16:36 +0100

    add hands and animate its position

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,6 +1,15 @@
 extends CharacterBody2D
 
 const SPEED = 100
+const HandYPosOffsetIdle = [0, 1, 1, 1]
+const HandYPosOffsetRun = [0, -1, 0, 1]
+var Hand1MarkerStartPos 
+var Hand2MarkerStartPos 
+var frame
+
+func _ready() -> void:
+	Hand1MarkerStartPos = $MarkerHand1.position
+	Hand2MarkerStartPos = $MarkerHand2.position
 
 func _physics_process(delta: float) -> void:
 
@@ -20,5 +29,27 @@ func _physics_process(delta: float) -> void:
 		$AnimatedSprite2D.flip_h = true
 	if input_vector.x > 0:
 		$AnimatedSprite2D.flip_h = false
+		
+	#change animation
+	if input_vector.x != 0 or input_vector.y != 0:
+		$AnimatedSprite2D.play("run")
+	else:
+		$AnimatedSprite2D.play("idle")
+		
+	# hand movement
+	if frame != $AnimatedSprite2D.frame:
+		#reset pos
+		$MarkerHand1.position = Hand1MarkerStartPos
+		$MarkerHand2.position = Hand2MarkerStartPos
+		
+		frame = $AnimatedSprite2D.frame
+		
+		if $AnimatedSprite2D.animation == "idle":
+			$MarkerHand1.position.y += HandYPosOffsetIdle[frame]
+			$MarkerHand2.position.y += HandYPosOffsetIdle[frame]
+			
+		if $AnimatedSprite2D.animation == "run":
+			$MarkerHand1.position.y += HandYPosOffsetRun[frame] - 2
+			$MarkerHand2.position.y += HandYPosOffsetRun[frame] - 2
 
 	move_and_slide()
